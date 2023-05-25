7f28ba7cd27d92bf9460474033e14f3453ba5fd8
supriya <supriya.s@zeomosolabs.com>
2023-05-25 16:44:33 +0530

    save

---
+++ src/main/java/myproject/demo/WelcomeController.java
@@ -0,0 +1,12 @@
+package myproject.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome new";
+    }
+}
