67c1cd907911707b11b0cc6f07486dd9b87532db
sandeep-zemoso <mudigondasandeep01@gmail.com>
2023-05-25 17:48:04 +0530

    my first spring boot application for sonar integration

--- src/main/java/myproject/demo/WelcomeController.java
+++ src/main/java/myproject/demo/WelcomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WelcomeController {
     @GetMapping("/welcome")
     public String welcome(){
-        return "Welcome new";
+        return "Welcome new test1";
     }
 }
