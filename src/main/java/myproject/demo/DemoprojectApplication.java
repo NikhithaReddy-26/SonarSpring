5f0f0570765613017330f9a0b6af9e9c8c57a957
nikhitha <nikhitha.thimmannagaari@gmail.com>
2023-05-29 08:58:29 +0530

    first sonar project in spring boot

--- src/main/java/myproject/demo/DemoprojectApplication.java
+++ src/main/java/myproject/demo/DemoprojectApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoprojectApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(DemoprojectApplication.class, args);
 	}
 
