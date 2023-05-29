944f3e9f9759837243829bd1d0ba6728878c5447
nikhitha <nikhitha.thimmannagaari@gmail.com>
2023-05-29 10:06:01 +0530

    sonar integration for spring

--- src/main/java/myproject/demo/HelloController.java
+++ src/main/java/myproject/demo/HelloController.java
@@ -2,11 +2,12 @@ package myproject.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
-public class WelcomeController {
+public class HelloController {
+
+
+
     @GetMapping("/welcome")
     public String welcome(){
         return "This is first application";
-    }
-}
+    }}
